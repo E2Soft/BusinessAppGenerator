7777619e3670d59d59ae608c8e8edd31de4cefb7
Milos Simic <milossimicsimo@gmail.com>
2015-03-15 15:07:59 +0100

    [NEW]Custom tagovi i filteri

---
+++ ReferentImplementation/states/templatetags/state_extras.py
@@ -0,0 +1,42 @@
+'''
+Created on Mar 3, 2015
+
+@author: Milos
+'''
+from django import template
+from django.core.urlresolvers import reverse
+
+from states.models import Store, City, State
+from builtins import str
+
+
+register = template.Library()
+
+@register.filter
+def classname(obj):
+    return obj.__class__.__name__ != "QuerySet"
+
+@register.filter
+def filternormal(obj):
+    filtered = [ (key,value) for key,value in obj.items() if value.__class__.__name__ != "QuerySet"]
+    return filtered
+
+@register.filter
+def filterlists(obj):
+    filtered = [ (key,value) for key,value in obj.items() if value.__class__.__name__ == "QuerySet"]
+    return filtered
+
+@register.filter
+def filterurl(obj):
+    if isinstance(obj, Store):
+        return reverse('detailstore',args=(obj.id,))
+    elif isinstance(obj, City):
+        return reverse('detailcity',args=(obj.id,))
+    elif isinstance(obj, State):
+        return reverse('detailstate',args=(obj.id,))
+    else:
+        return
+    
+@register.filter
+def isstring(obj):
+    return isinstance(obj, str)
