5f708743355eae80b2ade146f24a040cf417b331
Milos Simic <milossimicsimo@gmail.com>
2015-03-15 15:44:40 +0100

    [NEW]Detalji za korisnika [NEW]Izmena korisnika

--- ReferentImplementation/MBRSReferentImplementation/forms.py
+++ ReferentImplementation/MBRSReferentImplementation/forms.py
@@ -10,3 +10,44 @@ class DetailUser(DetailView):
     model = User
     template_name = 'states/udetail.html'
     context_object_name='user'
+    
+
+class RegistrationForm(UserCreationForm):
+    email = forms.EmailField(required=True)
+    first_name = forms.CharField(required = False)
+    last_name = forms.CharField(required = False)
+    
+    class Meta:
+        model = User
+        fields = ['first_name','last_name', 'username', 'email', 'password1', 'password2']
+    
+    def __init__(self, *args, **kwargs):
+        super(RegistrationForm, self).__init__(*args, **kwargs)
+        for field in self.fields.values():
+            field.widget.attrs['class']='form-control'
+
+class UserForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ['first_name','last_name', 'username', 'email']
+    
+    def __init__(self, *args, **kwargs):
+        super(UserForm, self).__init__(*args, **kwargs)
+        for field in self.fields.values():
+            field.widget.attrs['class']='form-control'
+
+class UserCreate(CreateView):
+    model = User
+    template_name = 'states/register.html'
+    form_class = RegistrationForm
+    
+    def get_success_url(self):
+        return reverse('home')
+        
+class UserUpdate(UpdateView):
+    model = User
+    template_name = 'states/uupdate.html'
+    form_class = UserForm
+    
+    def get_success_url(self):
+        return reverse('udetail',args=(self.get_object().id,))
--- ReferentImplementation/MBRSReferentImplementation/urls.py
+++ ReferentImplementation/MBRSReferentImplementation/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
-from MBRSReferentImplementation.forms import DetailUser
+from MBRSReferentImplementation.forms import DetailUser,UserUpdate
 
 urlpatterns = patterns('',
     # Examples:
@@ -9,4 +9,5 @@ urlpatterns = patterns('',
     url(r'^', include('states.urls')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^udetail/(?P<pk>\d+)/$', DetailUser.as_view(), name='udetail'),
+    url(r'uedit/(?P<pk>\d+)/$', UserUpdate.as_view(), name='uedit'),
 )
