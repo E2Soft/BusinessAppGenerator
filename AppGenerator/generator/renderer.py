8f3600e7294009e717478796c242ce74f289b86e
zeljko.bal <zeljko.bal@gmail.com>
2015-03-16 18:26:07 +0100

    - kostur generatora

---
+++ AppGenerator/generator/renderer.py
@@ -0,0 +1,18 @@
+'''
+Created on Mar 16, 2015
+
+@author: PCX
+'''
+
+def render(project_path, app_model):
+    '''
+    Generise fajlove na osnovu templejta i modela aplikacije.
+    '''
+    render_models(project_path, app_model)
+    render_views(project_path, app_model)
+
+def render_models(project_path, app_model):
+    pass
+
+def render_views(project_path, app_model):
+    pass
