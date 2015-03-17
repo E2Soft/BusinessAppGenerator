b955c53ad2e546f76815a1d22a667199f24efbc0
zeljko.bal <zeljko.bal@gmail.com>
2015-03-17 12:09:30 +0100

    - izmenjen api za generator da moze da se prosledi samo model

--- AppGenerator/generator/generator.py
+++ AppGenerator/generator/generator.py
@@ -5,14 +5,15 @@ Created on Mar 16, 2015
 '''
 from generator import specification, renderer
 
-def generate_app(project_path, xml_model_path):
+def generate_app_from_xml(project_path, xml_model_path):
     '''
     Generise aplikaciju na zadatoj lokaciji sa zadatim xml modelom aplikacije.
     '''
     app_model = specification.from_xml_file(xml_model_path)
+    generate_app(project_path, app_model)
     
+def generate_app(project_path, app_model):
     copy_static_files(project_path)
-    
     renderer.render(project_path, app_model)
 
 def copy_static_files(project_path):
