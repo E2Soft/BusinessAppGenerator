1c765d3750d58c2116e8008d12d43fddb32880ca
zeljko.bal <zeljko.bal@gmail.com>
2015-03-17 12:36:55 +0100

    - dodat jinja Environment u renderer i templates folder

--- AppGenerator/generator/renderer.py
+++ AppGenerator/generator/renderer.py
@@ -4,6 +4,10 @@ Created on Mar 16, 2015
 @author: PCX
 '''
 
+from jinja2 import Environment, PackageLoader
+
+env=Environment(loader=PackageLoader('generator', 'templates'))
+
 def render(project_path, app_model):
     '''
     Generise fajlove na osnovu templejta i modela aplikacije.
@@ -12,7 +16,9 @@ def render(project_path, app_model):
     render_views(project_path, app_model)
 
 def render_models(project_path, app_model):
+    #env.get_template('models.py.html').render(the='variables', go='here')
     pass
 
 def render_views(project_path, app_model):
+    #env.get_template('views.py.html').render(the='variables', go='here')
     pass
