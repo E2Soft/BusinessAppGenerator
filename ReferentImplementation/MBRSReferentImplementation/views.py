df12f36666b49a40a61f8bd511151482a019cce6
Milos Simic <milossimicsimo@gmail.com>
2015-03-17 10:32:20 +0100

    [NEW]Dodat view i url za logout

--- ReferentImplementation/MBRSReferentImplementation/views.py
+++ ReferentImplementation/MBRSReferentImplementation/views.py
@@ -4,7 +4,27 @@ Created on Mar 3, 2015
 @author: Milos
 '''
 from django.shortcuts import render
-
+from django.contrib import auth
 
 def home(request):
-    return render(request,'states/main.html')
+    if request.user.is_authenticated():
+        context = {"isadmin":request.user.is_superuser,"user":request.user}
+        return render(request,'states/main.html',context)
+     
+    username = request.POST.get("username","")
+    password = request.POST.get("password","")
+    
+    user = auth.authenticate(username=username, password=password)
+    context = {"isadmin":request.user.is_superuser,"user":request.user}
+    
+    if user is not None:
+        auth.login(request, user)
+
+        return render(request,'states/main.html',context) 
+     
+    else:
+        return render(request,'states/login.html')
+    
+def logout(request):
+    auth.logout(request)
+    return render(request, 'states/login.html')
