fa29d37a656d2c8437b1a7f826a93f868ddc7e8c
Milos Simic <milossimicsimo@gmail.com>
2015-03-18 11:32:56 +0100

    [UPDATE]test model dodate veze i jos dodatnih atributa [NEW] model tempate koji nasledjuje base i dodaje generisanje importa za modele

--- AppGenerator/test/generator_test.py
+++ AppGenerator/test/generator_test.py
@@ -6,17 +6,43 @@ Created on Mar 17, 2015
 import unittest
 
 from generator import generator
-from test.model import AppModel, Form, Field
+from test.model import AppModel, Form, Field, Link
 
 
 class Test(unittest.TestCase):
 
     def setUp(self):
         self.app_model = AppModel(app_name='My test app', 
-                                  forms=[Form(title='First form', 
-                                              fields=[Field(name='field1', label='My Field 1', field_type='String'),
-                                                      Field(name='field2', label='My Field 2', field_type='String'),
-                                                     ])
+                                  forms=[Form(title='Firstform', 
+                                              fields=[Field(name='field1', label='My Field 1', field_type='String',
+                                                            mandatory=True,max_length=4),
+                                                      Field(name='field2', label='My Field 2', field_type='String',
+                                                            mandatory=False, max_length=10),
+                                                      Field(name='field3', label='My Field 2', field_type='Integer',
+                                                            mandatory=False),
+                                                     ]),
+                                         Form(title='Secondform', 
+                                              fields=[Field(name='field3', label='My Field 3', field_type='String',
+                                                            mandatory=True,max_length=4),
+                                                      Field(name='field4', label='My Field 4', field_type='String',
+                                                            mandatory=False, max_length=10),
+                                                      Link(name="vezica",label='Veza', field_type="Link",mandatory=False,
+                                                           form="Firstform",link_type="1-*")
+                                                     ]),
+                                         Form(title='Thirdform', 
+                                              fields=[Field(name='field1', label='My Field 3', field_type='String',
+                                                            mandatory=True,max_length=4),
+                                                      Field(name='field2', label='My Field 4', field_type='String',
+                                                            mandatory=False, max_length=10),
+                                                      Link(name="vezica",label='Veza', field_type="Link",mandatory=False,
+                                                           form="Firstform",link_type="*-*"),
+                                                      Link(name="vezica3",label='Veza', field_type="Link",mandatory=True,
+                                                           form="Secondform",link_type="1-1"),
+                                                      Field(name='field4', label='My Field 4', field_type='Float',
+                                                            mandatory=True),
+                                                      Field(name='field5', label='My Field 4', field_type='DateTime',
+                                                            mandatory=False),
+                                                     ]),
                                         ])
 
     def test_generator(self):
