d07a04f14a1dee3035d02d529ab521c9e2078938
Milos Simic <milossimicsimo@gmail.com>
2015-03-18 11:30:36 +0100

    [UPDATE] renderer dodati delovi za generisanje ostatka potrebnih fajlova [UPDATE]Dodata funkcionalnost generianja models.py [NEW]base template koji genrise osnovu tj tekst ko je generisao i kada i da je zabranjeno menjanje generisanjih fajlova [NEW] models template iz kog se generise models.py na osnovu modela iz xml-a [NEW] admin template iz kog se generise admin.py na osnovu modela iz xml-a [UPDATE]model dodata klasa za vezu

--- AppGenerator/generator/renderer.py
+++ AppGenerator/generator/renderer.py
@@ -4,7 +4,11 @@ Created on Mar 16, 2015
 @author: PCX
 '''
 
+import datetime
+import getpass
+
 from jinja2 import Environment, PackageLoader
+
 
 env=Environment(loader=PackageLoader('generator', 'templates'))
 
@@ -14,11 +18,29 @@ def render(project_path, app_model):
     '''
     render_models(project_path, app_model)
     render_views(project_path, app_model)
+    render_admin(project_path, app_model)
+    render_urls(project_path, app_model)
+    render_forms(project_path, app_model)
+    print("JOB DONE")
 
 def render_models(project_path, app_model):
-    #env.get_template('models.py.html').render(the='variables', go='here')
-    pass
+    renderedmodel = env.get_template('models').render(model = app_model,datetime=datetime.datetime.now(),
+                                                      guest=getpass.getuser())
+    with open(project_path+"/models.py", "w") as file:
+        file.write(renderedmodel)
 
 def render_views(project_path, app_model):
     #env.get_template('views.py.html').render(the='variables', go='here')
+    pass
+
+def render_admin(project_path, app_model):
+    #env.get_template('models.py.html').render(the='variables', go='here')
+    pass
+
+def render_urls(project_path, app_model):
+    #env.get_template('models.py.html').render(the='variables', go='here')
+    pass
+
+def render_forms(project_path, app_model):
+    #env.get_template('models.py.html').render(the='variables', go='here')
     pass
--- AppGenerator/test/model.py
+++ AppGenerator/test/model.py
@@ -14,8 +14,20 @@ class Form:
         self.title=title
         self.fields=fields
 
+class Link:
+    def __init__(self, name=None,label=None, field_type=None,mandatory=False,form=None,link_type=None):
+        self.field_type = field_type
+        self.mandatory = mandatory
+        self.form = form
+        self.label=label
+        self.name = name
+        self.link_type = link_type
+
 class Field:
-    def __init__(self, name=None, label=None, field_type=None):
+    def __init__(self, name=None, label=None, field_type=None, mandatory = True, max_length=0,link=None):
         self.name=name
         self.label=label
         self.field_type=field_type
+        self.mandatory = mandatory
+        self.max_length = max_length
+        self.link = link
