1b81daa00eeb151edefd4f6f32d60c4e1cfca050
zeljko.bal <zeljko.bal@gmail.com>
2015-03-18 18:47:24 +0100

    - kopiranje statickih fajlova iz 'static' direktorijuma u generator paketu

--- AppGenerator/generator/generator.py
+++ AppGenerator/generator/generator.py
@@ -4,15 +4,22 @@ Created on Mar 16, 2015
 @author: PCX
 '''
 from generator import specification, renderer
+import os, shutil
 
-def generate_app_from_xml(project_path, xml_model_path):
+static_files_path = os.path.join(os.path.dirname(__file__), 'static')
+
+def generate_app_from_xml(path, xml_model_path):
     '''
     Generise aplikaciju na zadatoj lokaciji sa zadatim xml modelom aplikacije.
     '''
     app_model = specification.from_xml_file(xml_model_path)
-    generate_app(project_path, app_model)
+    generate_app(path, app_model)
     
-def generate_app(project_path, app_model):
+def generate_app(path, app_model):
+    '''
+    Generise aplikaciju na zadatoj lokaciji sa zadatim modelom aplikacije.
+    '''
+    project_path = os.path.join(path, app_model.app_name.replace(' ', '_')) # root folder na osnovu imena aplikacije (razmak zamenjen sa _)
     copy_static_files(project_path)
     renderer.render(project_path, app_model)
 
@@ -20,4 +27,6 @@ def copy_static_files(project_path):
     '''
     Kopira strukturu direktorijuma projekta i staticke (negenerisane) fajlove.
     '''
-    pass
+    if os.path.exists(project_path):
+        shutil.rmtree(project_path)
+    shutil.copytree(static_files_path, project_path)
