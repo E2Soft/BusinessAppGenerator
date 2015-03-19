c2678ab800537439a30d3699e2486d4093235fe3
zeljko.bal <zeljko.bal@gmail.com>
2015-03-19 18:18:46 +0100

    - procesuiranje xml modela da izgleda kao objektni test model - razresavanje referenci kod linkova

--- AppGenerator/generator/specification.py
+++ AppGenerator/generator/specification.py
@@ -8,7 +8,38 @@ from lxml import objectify
 
 def from_xml_file(file_path):
     with open(file_path, 'r') as fileobject:
-        return objectify.parse(fileobject).getroot()
+        app_model = objectify.parse(fileobject).getroot()
+        return _process_model(app_model)
     
 def from_xml_string(xml):
-    return objectify.fromstring(xml)
+    app_model = objectify.fromstring(xml)
+    return _process_model(app_model)
+
+def _process_model(app_model):
+    
+    _resolve_links(app_model)
+    
+    app_model.forms = [_process_form(f) for f in app_model.forms.Form]
+    
+    return app_model
+
+def _process_form(form):
+    if hasattr(form.fields, 'FormField'):
+        form_fields = [f for f in form.fields.FormField]
+    else:
+        form_fields = []
+    if hasattr(form.fields, 'LinkField'):
+        link_fields = [f for f in form.fields.LinkField]
+    else:
+        link_fields = []
+    
+    form.fields = form_fields + link_fields
+    
+    return form
+
+def _resolve_links(app_model):
+    for form in app_model.forms.Form:
+        if hasattr(form.fields, 'LinkField'):
+            for link in form.fields.LinkField:
+                link.form = link.form.xpath(link.form.get('reference'))[0]
+
