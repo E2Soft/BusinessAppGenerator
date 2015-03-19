193460799a0c4d754ed533bbe94592710ea364bd
Milos Simic <milossimicsimo@gmail.com>
2015-03-19 19:07:31 +0100

    [NEW]Dodata funkcija koja generise django aplikaciju.Ako aplikacija postoji prepisace sve fajlove osim custom.py u kom se nalaze runo pisani kod.Ako aplikacija ne postoji napravice je i dodati sve fajlove i custom.py

--- AppGenerator/generator/renderer.py
+++ AppGenerator/generator/renderer.py
@@ -6,11 +6,14 @@ Created on Mar 16, 2015
 
 import datetime
 import getpass
+import os
 
 from jinja2 import Environment, PackageLoader
 
 
 env=Environment(loader=PackageLoader('generator', 'templates'))
+
+DJANGO_APP_NAME = "business_app"
 
 def render(project_path, app_model):
     '''
@@ -30,7 +33,7 @@ def render_models(project_path, app_model):
 
 def render_views(project_path, app_model):
     renderedviews = env.get_template('views').render(model = app_model,datetime=datetime.datetime.now(),
-                                                             guest=getpass.getuser(),app_name="business_app")
+                                                             guest=getpass.getuser(),app_name=DJANGO_APP_NAME)
     with open(project_path+"/views.py", "w") as file:
         file.write(renderedviews)
 
@@ -42,12 +45,22 @@ def render_admin(project_path, app_model):
 
 def render_urls(project_path, app_model):
     renderedurls = env.get_template('urls').render(model = app_model,datetime=datetime.datetime.now(),
-                                                     guest=getpass.getuser(),app_name="business_app")
+                                                     guest=getpass.getuser(),app_name=DJANGO_APP_NAME)
     with open(project_path+"/urls.py", "w") as file:
         file.write(renderedurls)
 
 def render_forms(project_path, app_model):
     renderedforms = env.get_template('forms').render(model = app_model,datetime=datetime.datetime.now(),
-                                                     guest=getpass.getuser(),app_name="business_app")
+                                                     guest=getpass.getuser(),app_name=DJANGO_APP_NAME)
     with open(project_path+"/forms.py", "w") as file:
         file.write(renderedforms)
+        
+def generate_django_app():
+    if not os.path.exists(DJANGO_APP_NAME): 
+        os.makedirs(DJANGO_APP_NAME)
+        
+        with open(DJANGO_APP_NAME+"/__init__.py", "w") as file:
+            file.write("")
+        
+        with open(DJANGO_APP_NAME+"/custom.py", "w") as file:
+            file.write("#Add custom functions here...See urls.py for more info!")
