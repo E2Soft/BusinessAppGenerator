4d2731d205f88f7f07f8ce6bfe02d36521e70448
zeljko.bal <zeljko.bal@gmail.com>
2015-03-25 20:25:15 +0100

    - dodat cli - dodate opcije da se bekapuje db i migracije (po defaultu se bekapuje)

--- AppGenerator/generator/app_generator.py
+++ AppGenerator/generator/app_generator.py
@@ -8,38 +8,48 @@ from generator import specification, renderer
 import os, shutil
 
 static_files_path = os.path.join(os.path.dirname(__file__), 'static')
-temp_path = os.path.join(os.path.dirname(__file__), 'temp')
-temp_custom_code_path = os.path.join(temp_path, 'custom.py')
+temp_dir_path = os.path.join(os.path.dirname(__file__), 'temp')
 
-def generate_app_from_xml(path, xml_model_path):
+def generate_app_from_xml(path, xml_model_path=None, xml_model_string=None, **kwargs):
     '''
     Generise aplikaciju na zadatoj lokaciji sa zadatim xml modelom aplikacije.
     '''
-    app_model = specification.from_xml_file(xml_model_path)
-    generate(path, app_model)
+    if (xml_model_path and xml_model_string) or not(xml_model_path or xml_model_string):
+        raise Exception('Provide either xml_model_path or xml_model_string.')
+    if xml_model_path:
+        app_model = specification.from_xml_file(xml_model_path)
+    if xml_model_string:
+        app_model = specification.from_xml_string(xml_model_string)
+    generate(path, app_model, **kwargs)
     
-def generate(path, app_model):
+    return app_model
+    
+def generate(path, app_model, **kwargs):
     '''
     Generise aplikaciju na zadatoj lokaciji sa zadatim modelom aplikacije.
     '''
     project_app_name = app_model.app_name.replace(' ', '_')
     project_path = os.path.join(path, project_app_name) # root folder na osnovu imena aplikacije (razmak zamenjen sa _)
     
-    copy_static_files(project_path, project_app_name)
+    copy_static_files(project_path, project_app_name, **kwargs)
     renderer.render(project_path, app_model, project_app_name)
 
-def copy_static_files(project_path, project_app_name):
+def copy_static_files(project_path, project_app_name, **kwargs):
     '''
     Kopira strukturu direktorijuma projekta i staticke (negenerisane) fajlove.
     '''
+    rewrite_db = kwargs.get('rewrite_db')
+    rewrite_migrations = kwargs.get('rewrite_migrations')
     
-    custom_code_path = os.path.join(project_path, 'business_app', 'custom.py')
+    app_path = os.path.join(project_path, 'business_app')
     
     # remove directory if exists
     if os.path.exists(project_path):
-        # save custom.py
-        if os.path.exists(custom_code_path):
-            shutil.move(custom_code_path, temp_custom_code_path)
+        backup(app_path, 'custom.py')
+        if not rewrite_db:
+            backup(project_path, 'db.sqlite3')
+        if not rewrite_migrations:
+            backup(app_path, 'migrations')
         # remove old project
         shutil.rmtree(project_path)
         
@@ -47,9 +57,22 @@ def copy_static_files(project_path, project_app_name):
     shutil.copytree(static_files_path, project_path)
     
     # move saved custom.py
-    if os.path.exists(temp_custom_code_path):
-        shutil.move(temp_custom_code_path, custom_code_path)
+    restore(app_path, 'custom.py')
+    restore(project_path, 'db.sqlite3')
+    restore(app_path, 'migrations')
     
     # rename app directory
     os.rename(os.path.join(project_path, '__app__'), os.path.join(project_path, project_app_name))
+
+def backup(dir_path, name):
+    temp_path = os.path.join(temp_dir_path, name)
+    target_path = os.path.join(dir_path, name)
+    if os.path.exists(target_path):
+        shutil.move(target_path, temp_path)
+
+def restore(dir_path, name):
+    temp_path = os.path.join(temp_dir_path, name)
+    target_path = os.path.join(dir_path, name)
+    if os.path.exists(temp_path):
+        shutil.move(temp_path, target_path)
     
