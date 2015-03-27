6f6e89f4019a02e8e3e14a08aa2eb18d894449c8
Milos Simic <milossimicsimo@gmail.com>
2015-03-27 21:47:22 +0100

    [NEW]Dodato popunjavanje centralnog panela dummy podacima

--- AppGenerator/generator/static/business_app/static/business_app/actions.js
+++ AppGenerator/generator/static/business_app/static/business_app/actions.js
@@ -48,6 +48,25 @@ window.onload = function(){
 	        		counter++;
 	        	}
 	   			counter++;
+	   			
+	   			//popuni centralni panel
+	   			var centar = data["centar"];
+	        	
+	        	for(var index in centar){
+	        		var prop = centar[index];//elem
+	        		
+	        		$("#centarPanel").append("<div class='box'><ul class='list-group' id='box" + counter + "'>");
+	        		for(var i in prop){
+	        			if(i != "title"){
+	        				$("#box"+counter).append("<li class='list-group-item'><span class='glyphicon glyphicon-chevron-right'></span> "+prop[i]+"</li>");
+	        			}else{
+	        				$("#box"+counter).append("<li class='list-group-item list-group-item-info'><b><span class='glyphicon glyphicon-certificate'></span><a href='"+prop[i][1]+"'> "+prop[i][0]+"</a></b></li>");
+	        			}
+	        		}
+	        		$("#centarPanel").append("</ul></div>");
+	        		counter++;
+	        	}
+	   			counter++;
 	        },
 	        error: function(){
 	        	alert("error");
