ca6d1dfa59841b036aa5d052ab2ed63ec0e74da6
Milos Simic <milossimicsimo@gmail.com>
2015-03-27 17:12:43 +0100

    [UPDATE]Action dodat ajax poziv za visuals [DELETE]custom templejt metoda visuals posto se sada radi preko ajax-a [UPDATE] project_view templejt posto vise ne koristmo stari nacin [NEW]custom templejt dodat poziv ajx-a [NEW]urls templejt dodat hendler za ajax

--- AppGenerator/generator/static/business_app/static/business_app/actions.js
+++ AppGenerator/generator/static/business_app/static/business_app/actions.js
@@ -5,3 +5,25 @@ $(document).ready(function() {
 $(function () { 
 	$("[data-toggle='tooltip']").tooltip(); 
 });
+
+window.onload = function(){
+	var placeholder = document.getElementById("placeHolder");
+	if(placeholder != null){
+		$.ajax({
+	    	type: 'GET',
+	        dataType: 'json',
+	        data : {
+	        	'pk' : '1'
+	        },
+	        url: '/visuals/',
+	        success: function(data){
+	        var json = JSON.stringify(data);
+	        	alert(json);
+	        	alert(data["hello"]);
+	        },
+	        error: function(){
+	        	alert("error");
+	        }
+		});
+	}
+}
