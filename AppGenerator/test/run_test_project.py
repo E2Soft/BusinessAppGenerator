21c1e3d1e2239bcbb022352e7f8921a9c65885ad
zeljko.bal <zeljko.bal@gmail.com>
2015-03-31 01:28:03 +0200

    - integration

--- AppGenerator/test/run_test_project.py
+++ AppGenerator/test/run_test_project.py
@@ -10,7 +10,7 @@ from test import test_data
 from generator import app_generator, specification
 from generator.manage import Manager
 
-"""
+
 def generate_test_app(app_model):
     app_name=app_model.app_name.replace(' ', '_')
     project_path = os.path.join('test_gen', app_name)
@@ -23,21 +23,9 @@ def generate_test_app(app_model):
     manager.run_server()
 
 if __name__ == "__main__":
-    #app_model = test_data.test_app_model
-    app_model = specification.from_xml_string(test_data.test_app_string)
+    #app_model = test_data.test_app_model # TEST PYTHON MODEL
+    app_model = specification.from_xml_string(test_data.test_app_string) # TEST XML MODEL
     
     generate_test_app(app_model)
-"""
-    
-if __name__ == "__main__":
-    app_model = test_data.test_app_model
-    app_name=app_model.app_name.replace(' ', '_')
-    project_path = os.path.join('test_gen', app_name)
-    
-    app_generator.generate("test_gen", app_model)
-    
-    manager = Manager(project_path, app_name)
-    manager.migrate_database()
-    manager.create_super_user(username='a', password='a')
-    manager.run_server()
+
     
