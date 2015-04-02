56754e673a2ce0bbebccee7caccc268654214ebe
zeljko.bal <zeljko.bal@gmail.com>
2015-04-02 19:27:28 +0200

    - dodat tooltip u md plugin

--- GeneratorSpecPlugin/src-test/test/XMLTest.java
+++ GeneratorSpecPlugin/src-test/test/XMLTest.java
@@ -30,21 +30,21 @@ public class XMLTest
 		operations.add(new Operation("Operation1", "Operation1", "nesto", true));
 		operations.add(new Operation("Operation2", "Operation2", "nesto2", false));
 		
-		Form form1 = new Form("Form_1", "polje1", "Form 1", fields, operations);
+		Form form1 = new Form("Form_1", "polje1", "Form 1", "tooltip 1", fields, operations);
 		
 		fields = new ArrayList<Field>();
 		fields.add(new FormField("polje3", "Polje 3", "int", true, 5, 1, false, null));
 		fields.add(new FormField("polje4", "Polje 4", "string", false, 5, 2, false, null));
 		fields.add(new LinkField("forma1link", "Forma 1", "Link", true, 3, "Form_1", "1-1", "f veza 1"));
 		
-		Form form2 = new Form("Form_2", "polje3", "Form 2", fields, new ArrayList<Operation>());
+		Form form2 = new Form("Form_2", "polje3", "Form 2", "tooltip 1", fields, new ArrayList<Operation>());
 		
 		fields = new ArrayList<Field>();
 		fields.add(new FormField("polje3", "Polje 3", "int", true, 5, 1, false, null));
 		fields.add(new FormField("polje4", "Polje 4", "string", false, 5, 2, false, null));
 		fields.add(new LinkField("forma2link", "Forma 2", "Link", true, 3, "Form_2", "1-1", "f veza 2"));
 		
-		Form form3 = new Form("Form_3", "polje3", "Form 3", fields, new ArrayList<Operation>());
+		Form form3 = new Form("Form_3", "polje3", "Form 3", "tooltip 1", fields, new ArrayList<Operation>());
 		
 		forms.add(form1);
 		forms.add(form2);
